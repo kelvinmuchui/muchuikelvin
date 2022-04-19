52f6d35cf278a732a9f3b95f4d7067acc7dfe87c
kelvinmuchui <muchuikelvin423@gmail.com>
2022-04-19 21:24:32 +0300

    testmotiomal

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,79 +3,74 @@ import './portfolio.css'
 import IMG1 from '../../assets/airbnb.png'
 import IMG2 from '../../assets/nft.png'
 import IMG3 from '../../assets/Amazonclone.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Airbnb Clone ",
+    github: "https://github.com/kelvinmuchui",
+    demo: "https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/"
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: "Airbnb Clone ",
+    github: "https://github.com/kelvinmuchui",
+    demo: "https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/"
+  },
+  {
+    id: 1,
+    image: IMG1,
+    title: "Airbnb Clone ",
+    github: "https://github.com/kelvinmuchui",
+    demo: "https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/"
+  },
+  {
+    id: 1,
+    image: IMG3,
+    title: " Ecommerce website like amazon",
+    github: "https://github.com/kelvinmuchui",
+    demo: "https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/"
+  },
+  {
+    id: 1,
+    image: IMG2,
+    title: "BlockChain NFt Website Using Sanity ",
+    github: "https://github.com/kelvinmuchui",
+    demo: "https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/"
+  },
+  {
+    id: 1,
+    image: IMG1,
+    title: "Airbnb Clone ",
+    github: "https://github.com/kelvinmuchui",
+    demo: "https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/"
+  },
+]
 const Portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className ="container portfolio__container">
-        <article className ="portfolio__item">
-          <div className ="portfolio__item-image">
-            <img src={IMG1} alt="airbnb"/>
-          </div>
-          <h3>This is a Portfolio item title</h3>
-          <div className ="profile__item-cta">
-          <a className ="btn " href ="https://github.com/kelvinmuchui"  target='_blank' >GitHub</a>
-          <a  className ="btn btn-primary"href ="https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/" target='_blank' >Live Demo</a>
-
-          </div>
-          
-          </article>
-          <article className ="portfolio__item">
-          <div className ="portfolio__item-image">
-            <img src={IMG3} alt="airbnb"/>
-          </div>
-          <h3>This is a Portfolio item title</h3>
-          <div className ="profile__item-cta">
-          <a className ="btn " href ="https://github.com/kelvinmuchui"  target='_blank' >GitHub</a>
-          <a  className ="btn btn-primary"href ="https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/" target='_blank' >Live Demo</a>
-
-          </div>
-          </article>
-          <article className ="portfolio__item">
-          <div className ="portfolio__item-image">
-            <img src={IMG1} alt="airbnb"/>
-          </div>
-          <h3>This is a Portfolio item title</h3>
-          <div className ="profile__item-cta">
-          <a className ="btn " href ="https://github.com/kelvinmuchui"  target='_blank' >GitHub</a>
-          <a  className ="btn btn-primary"href ="https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/" target='_blank' >Live Demo</a>
-
-          </div>
-          </article>
-          <article className ="portfolio__item">
-          <div className ="portfolio__item-image">
-            <img src={IMG2} alt="airbnb"/>
-          </div>
-          <h3>This is a Portfolio item title</h3>
-          <div className ="profile__item-cta">
-          <a className ="btn " href ="https://github.com/kelvinmuchui"  target='_blank' >GitHub</a>
-          <a  className ="btn btn-primary"href ="https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/" target='_blank' >Live Demo</a>
-
-          </div>
-          </article>
-          <article className ="portfolio__item">
-          <div className ="portfolio__item-image">
-            <img src={IMG1} alt="airbnb"/>
-          </div>
-          <h3>This is a Portfolio item title</h3>
-          <div className ="profile__item-cta">
-          <a className ="btn " href ="https://github.com/kelvinmuchui"  target='_blank' >GitHub</a>
-          <a  className ="btn btn-primary"href ="https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/" target='_blank' >Live Demo</a>
-
-          </div>
-          </article>
-          <article className ="portfolio__item">
-          <div className ="portfolio__item-image">
-            <img src={IMG2} alt="airbnb"/>
-          </div>
-          <h3>This is a Portfolio item title</h3>
-          <div className ="profile__item-cta">
-          <a className ="btn " href ="https://github.com/kelvinmuchui"  target='_blank' >GitHub</a>
-          <a  className ="btn btn-primary"href ="https://nftm-arket-place-ppgpce9wx-kelvinmuchui.vercel.app/" target='_blank' >Live Demo</a>
-
-          </div>
-          </article>
+       {
+         data.map(({id, image, title,github,demo})=>{
+           return(
+            <article key={id} className ="portfolio__item">
+            <div className ="portfolio__item-image">
+              <img src={image} alt="airbnb"/>
+            </div>
+            <h3>{title}</h3>
+            <div className ="profile__item-cta">
+            <a className ="btn " href ={github}  target='_blank' >GitHub</a>
+            <a  className ="btn btn-primary"href ={demo} target='_blank' >Live Demo</a>
+  
+            </div> 
+            </article>
+           )
+         })
+       }
           </div>
     </section>
   )
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,77 @@
 import React from 'react'
 import "./testimonials.css"
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+const data =[
+  {
+    avatar: AVTR1,
+    name: "Naomy Langat",
+    review:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aut quasi odit, alias porro odio? Magni consequuntur, ea, facere impedit magnam dolor expedita animi fugiat eius nihil ab, consequatur enim."
+  },
+  {
+    avatar: AVTR2,
+    name: "Sarah Wakaba",
+    review:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aut quasi odit, alias porro odio? Magni consequuntur, ea, facere impedit magnam dolor expedita animi fugiat eius nihil ab, consequatur enim."
+  },
+  {
+    avatar: AVTR3,
+    name: "Simon Njoroge",
+    review:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aut quasi odit, alias porro odio? Magni consequuntur, ea, facere impedit magnam dolor expedita animi fugiat eius nihil ab, consequatur enim."
+  },
+  {
+    avatar: AVTR4,
+    name: "Appolo Warui ",
+    review:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque aut quasi odit, alias porro odio? Magni consequuntur, ea, facere impedit magnam dolor expedita animi fugiat eius nihil ab, consequatur enim."
+  },
+
+]
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id="testimonials">
+      <h5>Review From clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper className ="container testimonials_container"
+       modules={[Navigation, Pagination, Scrollbar, A11y]}
+       spaceBetween={40}
+       slidesPerView={1}
+       navigation
+       pagination={{ clickable: true }}
+       scrollbar={{ draggable: true }}
+       onSwiper={(swiper) => console.log(swiper)}
+       onSlideChange={() => console.log('slide change')}
+      
+ >
+        {
+          data.map(({avatar, name, review}, index)=> {
+            return(
+              <SwiperSlide key = {index}className ="testimonial">
+          <div className ="client__avatar">
+            <img src ={avatar} alt =""/>
+            
+           
+          </div>
+          <h5 className ="client__name">{name}</h5>
+          <small className ="client__review">
+            {review}
+            </small>
+        </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
+    </section>
   )
 }
 
